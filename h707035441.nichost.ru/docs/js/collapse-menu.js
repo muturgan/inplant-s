46ce1bd3e3dfb1a8148fd59c0c9b036b849feaf1
muturgan <caxap_8787@mail.ru>
2018-07-04 14:47:13 +0300

    fix collapse menu on mobile device

---
+++ h707035441.nichost.ru/docs/js/collapse-menu.js
@@ -0,0 +1,19 @@
+"use strict";
+
+if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|BlackBerry|webOS|Windows Phone/i)) {
+  let collapseButton = document.body.querySelector('.navbar-toggle');
+  let menuSpase = document.body.querySelector('.navbar-collapse');
+
+  function handler(event) {
+    if (
+      !collapseButton.classList.contains('collapsed')
+      &&
+      event.target !== menuSpase
+    ) {
+      collapseButton.click();
+    }
+  }
+
+  document.body.addEventListener('click', handler);
+  //document.body.addEventListener('touch', handler);
+}
