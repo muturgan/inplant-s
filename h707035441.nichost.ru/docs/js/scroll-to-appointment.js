bb44482d487857ac8fbcd859c5320aecc8776062
muturgan <caxap_8787@mail.ru>
2018-07-04 16:45:59 +0300

    scrolling for .button with building. was added

--- h707035441.nichost.ru/docs/js/scroll-to-appointment.js
+++ h707035441.nichost.ru/docs/js/scroll-to-appointment.js
@@ -1,10 +1,14 @@
 "use strict";
 
+let scroll = 590;
+if (navigator.userAgent.match(/iPad|iPhone|iPod|Android|BlackBerry|webOS|Windows Phone/i)) {
+  scroll = 650;
+}
+
 const mediaIcon = document.body.querySelector('.media-icon');
 const appointmentField = document.body.querySelector('#appointment');
 
 mediaIcon.addEventListener('click', function(){
-  window.scrollTo(0, appointmentField.getBoundingClientRect().bottom)
-  //appointmentField.scrollIntoView(true);
+  window.scrollTo(0, scroll)
 });
 
